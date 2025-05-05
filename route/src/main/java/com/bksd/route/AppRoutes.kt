f4b1fcbc570517a0c8836268cf85c6a4f8249f94
Burak Karabekir <burakkarabekir@gmail.com>
2025-05-05 12:21:22 +0300

    add route for word detail screen

--- route/src/main/java/com/bksd/route/AppRoutes.kt
+++ route/src/main/java/com/bksd/route/AppRoutes.kt
@@ -1,5 +1,31 @@
 package com.bksd.route
 
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
 sealed class AppRoutes(val route: String) {
     data object Home : AppRoutes("home")
+    data object WordDetail : AppRoutes("word_detail/{word}/{pronunciation}") {
+        fun withArgs(
+            word: String,
+            pronunciation: String
+        ): String = "word_detail/${word.encode()}/${pronunciation.encode()}"
+
+        val arguments = listOf(
+            navArgument("word") { type = NavType.StringType },
+            navArgument("pronunciation") { type = NavType.StringType },
+        )
+    }
+
+    companion object {
+        // Optional: Utility to encode strings for safe URL usage
+        private fun String.encode(): String =
+            URLEncoder.encode(this, StandardCharsets.UTF_8.toString())
+
+        fun String.decode(): String =
+            URLDecoder.decode(this, StandardCharsets.UTF_8.toString())
+    }
 }

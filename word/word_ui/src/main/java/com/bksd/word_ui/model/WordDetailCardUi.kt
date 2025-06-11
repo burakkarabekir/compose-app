48624a1801f002873ca779bf7807b454f1dde23b
Burak Karabekir <burakkarabekir@gmail.com>
2025-06-11 15:50:20 +0300

    initialize values in WordDetailCardUi

--- word/word_ui/src/main/java/com/bksd/word_ui/model/WordDetailCardUi.kt
+++ word/word_ui/src/main/java/com/bksd/word_ui/model/WordDetailCardUi.kt
@@ -10,11 +10,11 @@ sealed interface WordDetailUi {
 }
 
 data class WordDetailCardUi(
-    override val word: String,
-    override val pronunciation: String?,
-    override val definition: String?,
-    override val synonyms: List<String>?,
-    override val antonyms: List<String>?,
+    override val word: String = "",
+    override val pronunciation: String? = "",
+    override val definition: String? = "",
+    override val synonyms: List<String>? = emptyList(),
+    override val antonyms: List<String>? = emptyList(),
     override val isFavorite: Boolean = false,
 ) : WordDetailUi
 
